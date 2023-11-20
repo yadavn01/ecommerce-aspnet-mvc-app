1b32eae314ed01167ade5f8804e0163a89cc31c8
yadavn01 <ynaman29@gmail.com>
2023-11-20 22:44:30 +1300

    adding model relationship complete

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -1,6 +1,32 @@
-﻿namespace eTickets.Data
+﻿using eTickets.Models;
+
+namespace eTickets.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId
+
+            });
+
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actor_Movies).HasForeignKey(m => m.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
     }
 }
